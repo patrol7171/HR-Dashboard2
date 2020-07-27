56b32b66ee57986afc2bb1a02de9d893297384a4
Patricia Rollins <patrol7171@gmail.com>
2020-07-27 11:49:19 -0400

    Update settings.py
    added db info

--- hr_dashboard2/settings.py
+++ hr_dashboard2/settings.py
@@ -23,9 +23,25 @@ else: # For Heroku only ->
     GEOCODIO_API_KEY = os.environ.get('Geocodio-API-Key')
     MAPBOX_API_KEY = os.environ.get('Mapbox-API-Key')
     ADMIN_PSWD = os.environ.get('Admin-Pswd')
-    DATABASES = {}
-    DATABASES['default'] = os.environ.get('JAWSDB_URL')
-    DATABASES['hr_data'] = os.environ.get('ELEPHANT_SQL_URL')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME':  'u7q2wl2obb7evh1n',                  
+            'USER': 'rezbtjsmxz1pmvji',
+            'PASSWORD': 'cqncgcjb4ma0r349',
+            'HOST': 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            'PORT': '3306',
+        },
+        'hr_data': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME':  'ebjjixcv',                  
+            'USER': 'ebjjixcv',
+            'PASSWORD': 'WvHOvTGbOReRZlUshx8gWp7S6ovr-uSh',
+            'HOST': 'ruby.db.elephantsql.com',
+            'PORT': '5432',
+        }
+    }
+
 
 
 
