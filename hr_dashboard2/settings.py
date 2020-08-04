52794c01c0ad2fcd6015d31c5fb8d3e0e0420172
Patricia Rollins <patrol7171@gmail.com>
2020-08-04 10:03:47 -0400

    Updated settings and css files
    updated static file settings and css footer

--- hr_dashboard2/settings.py
+++ hr_dashboard2/settings.py
@@ -152,16 +152,16 @@ USE_TZ = True
 
 
 
-# Static/Media files (CSS, JavaScript, Images)
+# Static & Media files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
